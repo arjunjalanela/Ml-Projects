7244f714d56988948b1154bd57a9c8adec4a3e2c
Arjun Jalanela <arjunjalanela@gmail.com>
2025-10-06 21:41:46 +0530

    Done

--- src/utils.py
+++ src/utils.py
@@ -63,3 +63,9 @@ def load_object(file_path):
 
     except Exception as e:
         raise CustomException(e, sys)
+def load_objects(file_path):
+    try:
+        with open(file_path,"rb") as file_obj:
+            return dill.load(file_obj)
+    except Exception as e:
+        raise CustomException(e, sys)
